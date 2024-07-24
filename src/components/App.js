61073aec5bce4c9e9770e90b8eaa4df09ae68dd4
sagarbissoyi <sagarbissoyi10@gmail.com>
2024-07-24 14:23:51 +0530

    verify

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,10 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const handleClick = () => {
-    alert('Button clicked!');
-  };
-
   return (
     <div className="App">
-      <header>
-        <h1>Welcome to My React App</h1>
-      </header>
-      <main>
-        <p>I am learning React. My life is getting better.</p>
-        <button onClick={handleClick}>Click me!</button>
-      </main>
+      <h1>Learning React</h1>
+      <p>I am learning React. My life is getting better.</p>
     </div>
   );
 }
