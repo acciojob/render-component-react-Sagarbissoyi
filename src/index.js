c774555be41d2d5331900312df85985b3a980038
sagarbissoyi <sagarbissoyi10@gmail.com>
2024-07-24 14:43:50 +0530

    noway

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
